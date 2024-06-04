185644df2ef2a1cda6863f3c25e62fb32523f847
CodeX13 <uhnspark@gmail.com>
2024-06-05 00:51:11 +0100

    frontend setup

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,21 @@
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+import daisyui from "daisyui"
+
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  daisyui:{
+    "light",
+    "dark",    
+  }
+  plugins: [
+    daisyui
+  ],
+} satisfies Config;
